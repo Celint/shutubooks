f024a27c53ce73038a7fb9241cc44b8e3ab85ba5
shuxiaofang <15237681165@163.com>
2019-10-03 14:21:45 +0800

    emmm

--- reading/static/js/lbyjs.js
+++ reading/static/js/lbyjs.js
@@ -2,12 +2,18 @@
 KindEditor.ready(function (K) {
 	window.editor = K.create('#editor');
 });
+function getTime() {
+	var date = new Date();
+	var month = ("" + (date.getMonth() + 1)).length === 1 ? "0" + (date.getMonth() + 1) : "" + (date.getMonth() + 1);
+	var today = ("" + (date.getDate())).length === 1 ? "0" + (date.getDate()) : "" + (date.getDate());
+	var hours = ("" + (date.getHours())).length === 1 ? "0" + (date.getHours()) : "" + (date.getHours());
+	var minutes = ("" + (date.getMinutes())).length === 1 ? "0" + (date.getMinutes()) : "" + (date.getMinutes());
+	return date.getFullYear() + "-" + month + "-" + today+ " " + hours + ":" + minutes;
+}
 $(document).ready(function () {
 	$("#comment").click(function () {
-		var date = new Date();
 		var nc = $("#addContent").val();
-		alert("你还不回去睡觉？");
-		var txt = "<li class='list-group-item'><p>"+nc+"</p ></li>";
+		var txt = "<li class='list-group-item'><p>"+"兰彬炎："+nc+"</p><p>"+getTime()+"</p></li>";
 		$("#commentPlace").prepend(txt);
 	})
 })
